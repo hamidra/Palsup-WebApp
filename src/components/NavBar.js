59cf0c4294175588c251f4cc4925fe1817e402a1
hra <hamid.alipour@live.com>
2019-01-14 16:16:14 -0800

    add navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 const NavBar = () => (
-    <div className="row">
-    </div>
+    <nav class="navbar sticky-top navbar-expand-sm navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#myNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="navbar-nav mr-auto mt-2 mt-sm-0">
+                <li class="nav-item d-sm-none">
+                    <a class="nav-link" href="#">Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Message</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Notifications</a>
+                </li>
+                <li class="nav-item d-sm-none">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+            </ul>
+            <a class="nav-link d-none d-sm-inline-block" href="#">User</a>
+        </div>
+    </nav>
 );
 
 export default NavBar;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const SearchBar = ()=>(
-    <div className="row">
-
+const SearchBar = () => (
+    <div className="row justify-content-center align-items-center">
+        <label className="mb-3">Feeling like</label>
+        <div className="col-12 col-sm-9 input-group mb-3">
+            <input type="text" className="form-control" placeholder="Recipient's username"/>
+            <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button">Button</button>
+            </div>
+        </div>
     </div>
 );
 
