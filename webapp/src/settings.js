a4fb2de23f5f024a59dfe7d01d4938d1851f87cd
hra <hamid.alipour@live.com>
2021-01-26 18:21:00 -0800

    add new api + fix some issues

--- webapp/src/settings.js
+++ webapp/src/settings.js
@@ -1,2 +1,2 @@
-//export const backend_endpoint = "https://api.palsup.com";
-export const backend_endpoint = 'http://localhost:4000';
+export const backend_endpoint = 'https://api.palsup.com';
+//export const backend_endpoint = 'http://localhost:4000';
